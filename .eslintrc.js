16a8406679ed934d4733b94ddd3cd1c499db814e
pvaliani <pedram.homayoun-valiani@bbc.co.uk>
2022-10-25 12:21:37 +0100

    updates eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,27 @@
 module.exports = {
-  extends: [
-    'airbnb',
-    'plugin:prettier/recommended',
-    'plugin:jsx-a11y/recommended',
-  ],
+  extends: ["plugin:prettier/recommended", "plugin:jsx-a11y/recommended"],
   env: {
     es6: true,
     browser: true,
     jest: true,
     node: true,
   },
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ['prettier', 'json', 'jsx-a11y', 'import'],
+  plugins: ["prettier", "json", "jsx-a11y", "import"],
   rules: {
-    'react/jsx-props-no-spreading': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'linebreak-style': process.platform === 'win32' ? 'off' : ['error', 'unix'],
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-useless-path-segments': [
-      'error',
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "linebreak-style": process.platform === "win32" ? "off" : ["error", "unix"],
+    "import/no-extraneous-dependencies": "off",
+    "import/no-useless-path-segments": [
+      "error",
       {
         noUselessIndex: false,
       },
